cd51c0aba20adda06ed53ed33d367fa54ec08f8b
Alex Kauã Silva Garcia <alexkaua502@gmail.com>
2026-02-22 13:33:58 -0300

    Iniciando spring

---
+++ src/main/java/com/salessystemapi/sales_system_api/SalesSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.salessystemapi.sales_system_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesSystemApiApplication.class, args);
+	}
+
+}
