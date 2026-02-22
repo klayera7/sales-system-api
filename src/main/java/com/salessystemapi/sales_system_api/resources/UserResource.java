d71b6b3bda3c7b7f85cd72b9c77a9b25828808a5
Alex Kauã Silva Garcia <alexkaua502@gmail.com>
2026-02-22 14:10:53 -0300

    feat(resources): implement UserResource with GET /users

--- src/main/java/com/salessystemapi/sales_system_api/resources/UserResource.java
+++ src/main/java/com/salessystemapi/sales_system_api/resources/UserResource.java
@@ -1,4 +1,19 @@
 package com.salessystemapi.sales_system_api.resources;
 
+import com.salessystemapi.sales_system_api.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
 public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll() {
+        User u = new User(1L, "Alex", "alex@gmail.com", "12345", "999999999");
+        return ResponseEntity.ok().body(u);
+    }
+
 }
