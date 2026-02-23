e9a2f8fb47f475a2f43a98ae637329d725144a23
Alex Kauã Silva Garcia <alexkaua502@gmail.com>
2026-02-23 18:09:44 -0300

    feat(services): add findById method and GET /users/{id} endpoint

--- src/main/java/com/salessystemapi/sales_system_api/resources/UserResource.java
+++ src/main/java/com/salessystemapi/sales_system_api/resources/UserResource.java
@@ -1,19 +1,33 @@
 package com.salessystemapi.sales_system_api.resources;
 
 import com.salessystemapi.sales_system_api.entities.User;
+import com.salessystemapi.sales_system_api.services.UserServices;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserServices userServices;
+
     @GetMapping
-    public ResponseEntity<User> findAll() {
-        User u = new User(1L, "Alex", "alex@gmail.com", "12345", "999999999");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAll() {
+        List<User> list = userServices.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User obj = userServices.findById(id);
+        return ResponseEntity.ok().body(obj);
     }
 
 }
